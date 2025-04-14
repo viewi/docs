49dd7d0ae148804ca38d45ac38b6bedbf913149e
Ivan Voitovych <col606v@gmail.com>
2025-04-14 17:56:20 +0300

    forms example

---
+++ ui/examples/Forms/FormsExample.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace ExamplesUi\Forms;
+
+use Viewi\Components\BaseComponent;
+
+class FormsExample extends BaseComponent
+{
+    public string $example = 'intro';
+    
+    //example 1
+    public string $firstName = '';
+    public string $lastName = '';
+    
+    //example 2
+    public string $summary = '';
+}
